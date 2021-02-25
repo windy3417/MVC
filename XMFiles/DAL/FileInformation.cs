2285168431c007b5e2136cf67549448c1e153e80
三 苏 <windy3417@LIVE.COM>
2021-02-25 21:19:46 +0800

    创建文件信息实体类

---
+++ XMFiles/DAL/FileInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMFiles.DAL
+{
+  public  class FileInformation
+    {
+        int id { get; set; }
+        string FileName { get; set; }
+        string FileCategory { get; set; }
+        string DepartmentBelong { get; set; }
+        string CreatePerson { get; set; }
+        DateTime CreateDate { get; set; }
+    }
+}
